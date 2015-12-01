69a533c987ee6197feb552fff4dd0ddeb168512d
Kang <2881570+kangni@users.noreply.github.com>
2015-12-01 20:43:01 +0800

    uses unittest

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,6 +1,20 @@
 from selenium import webdriver
+import unittest
 
-brower = webdriver.Firefox()
-brower.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.brower = webdriver.Firefox()
+        self.brower.implicitly_wait(3)
 
-assert 'Django' in brower.title
+    def tearDown(self):
+        self.brower.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.brower.get('http://localhost:8000')
+
+        self.assertIn('To-Do', self.brower.title)
+        self.fail('Finish the test!')
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
