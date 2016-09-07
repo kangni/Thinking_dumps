c4fa0ca23590e12ab320d783c7f3b322d2e856cb
Kang <2881570+kangni@users.noreply.github.com>
2016-09-07 22:25:54 +0800

    orderedSequentailSearch

---
+++ Data_Structure_and_Algorithms/Search/orderedSequentialSearch.py
@@ -0,0 +1,16 @@
+def orderedSequentialSearch(a_list, item):
+    pos = 0
+    found = False
+    stop = False
+
+    while pos < len(a_list) and not found and not stop:
+        if a_list[pos] == item:
+            found = True
+        else:
+            if a_list[pos] > item:
+                stop = True
+            else:
+                pos += 1
+
+    return found
+    
