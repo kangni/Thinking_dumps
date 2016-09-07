f6d4a3a8c605a3254b973a8524dbe065abfec8fd
Kang <2881570+kangni@users.noreply.github.com>
2016-09-07 22:26:45 +0800

    reverse characters in a string

---
+++ Data_Structure_and_Algorithms/Stack/reverse_characters_in_a_string.py
@@ -0,0 +1,9 @@
+def revstring(mystr):
+    myStack = Stack()
+    for ch in mystr:
+        myStack.push(ch)
+    revstr = ''
+    while not myStack.isEmpty():
+        revstr = revstr + myStack.pop()
+
+    return revstr
