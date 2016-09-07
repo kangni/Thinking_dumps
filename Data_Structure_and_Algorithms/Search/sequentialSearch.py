e2d9c929c98b4bf37dff91a3b2836fbe6aa992ba
Kang <2881570+kangni@users.noreply.github.com>
2016-09-07 22:25:20 +0800

    SequentialSearch

---
+++ Data_Structure_and_Algorithms/Search/sequentialSearch.py
@@ -0,0 +1,11 @@
+def sequentialSearch(a_list, item):
+    pos = 0
+    found = False
+
+    while pos < len(a_list) and not found:
+        if a_list[post] == tiem:
+            found = True
+        else:
+            pos += 1
+
+    return found
