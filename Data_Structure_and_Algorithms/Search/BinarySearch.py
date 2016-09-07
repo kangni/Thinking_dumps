da502b09082ff18555c1835d7adfaef00cd8b36c
Kang <2881570+kangni@users.noreply.github.com>
2016-09-07 22:24:45 +0800

    BinarySearch

---
+++ Data_Structure_and_Algorithms/Search/BinarySearch.py
@@ -0,0 +1,17 @@
+def BinarySearch(a_list, item):
+    first = 0
+    last = len(a_list) - 1
+    found = False
+
+    while first <= last and not found:
+        middle = (first + last) >> 1
+        if item < a_list[middle]:
+            last = middle -1
+        elif item > a_list[middle]:
+            first = middle + 1
+        else:
+            return middle
+            found = True
+
+    return -1
+        
