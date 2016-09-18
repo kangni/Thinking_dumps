9539d228ced8dbc6dac30ba6ff63bfb891c0f235
Kang <2881570+kangni@users.noreply.github.com>
2016-09-18 20:08:42 +0800

    Quick Sort

--- Data_Structure_and_Algorithms/Sort/quickSearch.py
+++ Data_Structure_and_Algorithms/Sort/quickSearch.py
@@ -7,6 +7,7 @@ def quickSearch(seq):
         greater = quickSearch([x for x in seq[1:] if x >= pivot])
         return lesser + [pivot] + greater
 
+
 if __name__ == '__main__':
     seq = [4, 5, 67, 34, 1, 0, -45, -1, 87, -12]
     print(quickSearch(seq))
