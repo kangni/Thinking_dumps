f7edf28a5a110583f28bf88242d6eb619d9d999f
Kang <2881570+kangni@users.noreply.github.com>
2016-09-30 10:55:23 +0800

    42_1

---
+++ Coding_Interviews/42_1_reverseWordsInSentence.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+
+'''
+输入一个英文句子，翻转句子中单词的顺序，但单词内字符顺序不变。
+为简单起见，标点符号和普通字母一样处理。
+例如输入字符串"I am a student."，则输出"student. a am I"
+'''
+
+
+def reverseSentence1(s):
+    return ' '.join(reversed(s.split()))
