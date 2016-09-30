db1115eece97a58f11c0aa830085ab7094c6a5c9
Kang <2881570+kangni@users.noreply.github.com>
2016-09-30 10:50:00 +0800

    04

---
+++ Coding_Interviews/04_replaceBlank.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+
+'''
+替换空格
+
+实现一个函数，把字符串中的每个空格替换成"%20"
+例如输入"We are happy."
+输出"We%20are%20happy."
+'''
+
+class Solution:
+    def replaceBlank(self, s):
+        return s.replace(" ", "%20")
