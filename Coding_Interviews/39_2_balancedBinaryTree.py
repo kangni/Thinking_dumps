54e2eb43fa3441c78cc633187365325dd74b4d32
Kang <2881570+kangni@users.noreply.github.com>
2016-10-06 09:33:24 +0800

    Balanced Binary Tree

---
+++ Coding_Interviews/39_2_balancedBinaryTree.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+'''
+输入一棵二叉树的根结点，判断该树是不是平衡二叉树。
+
+平衡二叉树：任意结点的左右子树的深度相差不超过1
+
+'''
+
+
+class Node(object):
+    def __init__(self, data, left=None, right=None):
+        self.data = data
+        self.left = left
+        self.right = right
+
+
+def is_balanced(root):
+    if not root:
+        return True
+    else:
+        if is_balanced(root.left) and is_balanced(root.right):
+            return abs(depth(root.left) - depth(root.right)) <= 1
+        else:
+            return False
+
+
+def depth(root):
+    if not root:
+        return False
+    return max(depth(root.left), depth(root.right)) + 1
+
+
+if __name__ == '__main__':
+    tree = Node(1, Node(2, Node(4), Node(5, Node(7))), Node(3, Node(6)))
+    print(is_balanced(tree))
