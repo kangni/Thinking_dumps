5f5c75ed8affad04ad3a77c5c6d0f9ccd7e020f3
Kang <2881570+kangni@users.noreply.github.com>
2016-10-07 10:05:41 +0800

    PEP8

--- Coding_Interviews/11_power.py
+++ Coding_Interviews/11_power.py
@@ -1,33 +1,31 @@
-# coding: utf-8
+# -*- coding: utf-8 -*-
 
-'''
-数值的整数次方
+# 数值的整数次方
 
-实现函数double Power(double base, int exponent)，求base的exponent次方。
-不得使用库函数，同时不需要考虑大数问题。
-'''
+# 实现函数double Power(double base, int exponent)，求base的exponent次方。
+# 不得使用库函数，同时不需要考虑大数问题。
 
-def getPower(base, exponent):
+
+def get_power(base, exponent):
     absExponent = exponent
     if exponent < 0:
         absExponent = -exponent
-
-    result = powerWithUnsignedExponent(base, absExponent)
+    result = power_with_unsigned_exponent(base, absExponent)
     if exponent < 0:
         result = 1.0 / result
     return result
 
 
-def powerWithUnsignedExponent(base, exponent):
+def power_with_unsigned_exponent(base, exponent):
     if exponent == 0:
         return 1
     if exponent == 1:
         return base
-
-    result = getPower(base, exponent >> 1)
+    result = get_power(base, exponent >> 1)
     result *= result
     if exponent & 0x1 == 1:
         result *= base
-
-
     return result
+
+if __name__ == '__main__':
+    print(get_power(2, 10))
