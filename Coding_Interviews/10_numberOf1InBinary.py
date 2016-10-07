4ea694d47a65e0af46de659c7dc37bd9c38f6f26
Kang <2881570+kangni@users.noreply.github.com>
2016-10-07 09:56:43 +0800

    fix and PEP8

--- Coding_Interviews/10_numberOf1InBinary.py
+++ Coding_Interviews/10_numberOf1InBinary.py
@@ -1,23 +1,21 @@
-# coding: utf-8
+# -*- coding: utf-8 -*-
 
-'''
-二进制中 1 的个数
+# 二进制中 1 的个数
 
-输入一个整数，输出该数二进制表示中 1 的个数
-比如把 9 表示成二进制是 1001，有 2 位是 1.
-所以输出 2
-'''
+# 输入一个整数，输出该数二进制表示中 1 的个数
+
+#比如把 9 表示成二进制是 1001，有 2 位是 1.
+#所以输出 2
 
 
-def numberOf1(n):
+def number_of_1(n):
     count = 0
 
     while n:
         count += 1
-        n = (n - 1) & n
+        n &= (n - 1)
 
     return count
 
-
 if __name__ == '__main__':
-    print(numberOf1(9))
+    print(number_of_1(9))
