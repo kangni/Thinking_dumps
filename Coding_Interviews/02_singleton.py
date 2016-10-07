925b8d96819dadba26e740f4c79418b119a863a2
Kang <2881570+kangni@users.noreply.github.com>
2016-10-07 10:11:34 +0800

    PEP8

--- Coding_Interviews/02_singleton.py
+++ Coding_Interviews/02_singleton.py
@@ -1,30 +1,29 @@
-# coding: utf-8
+# -*- coding: utf-8 -*-
 
-'''
-实现 Singleton 模式
 
-设计一个类，我们只能生成该类的一个实例。
-只能生成一个实例的类是实现了 Singleton (单例)模式的类型。
+# 实现 Singleton 模式
 
-在常用的模式中，Singleton 是唯一一个能够用短短几十行代码完整实现的模式。
-因此，写一个 Singleton 的类型是一个很常见的面试题。
+# 设计一个类，我们只能生成该类的一个实例。
+# 只能生成一个实例的类是实现了 Singleton (单例)模式的类型。
 
-'''
+# 在常用的模式中，Singleton 是唯一一个能够用短短几十行代码完整实现的模式。
+# 因此，写一个 Singleton 的类型是一个很常见的面试题。
+
 
 # 1、使用 __new__ 方法
-class singleton(object):
+class Singleton(object):
     def __new__(cls, *args, **kwargs):
-    # __new__()在__init__()之前被调用，用于生成实例对象
+        # __new__()在__init__()之前被调用，用于生成实例对象
         if not hasattr(cls, '_instance'):
-        # 说明该类还没有实例化
-            orig = super(singleton, cls)
+            # 说明该类还没有实例化
+            orig = super(Singleton, cls)
             cls._instance = orig.__new__(cls, *args, **kwargs)
             # 实例化该类并将实例绑到cls.instance
         return cls._instance
         # 以后每次实例化都返回第一次实例化创建的实例
 
 
-class MyClass(singleton):
+class MyClass(Singleton):
     a = 1
 
 
@@ -33,8 +32,6 @@ if __name__ == '__main__':
     b = MyClass()
     print(id(a), id(b))
 
-
-    
 '''
 # 2、共享属性
 # 创建实例时把所有实例的 __dict__ 指向同一个字典，这样它们具有相同的属性和方法
@@ -57,26 +54,22 @@ if __name__ == '__main__':
 
 '''
 
-
-
 '''
 # 3、装饰器版本
 
 def singleton(cls, *args, **kwargs):
     instances = {}
-    def getinstance():
+    def get_instance():
         if cls not in instances:
             instances[cls] = cls(*args, **kwargs)
         return instances[cls]
-    return getinstance
+    return get_instance
 
 @singleton
 class MyClass():
     pass
 
 '''
-
-
 
 '''
 ＃ 4、import 方法: 作为 Python 的模块是天然的单例模式
@@ -90,6 +83,4 @@ my_singleton = My_Singleton()
 from mysingleton import my_singleton
 
 my_singleton.foo()
-
 '''
-
--- Coding_Interviews/16_ReverseList.py
+++ Coding_Interviews/16_ReverseList.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+
 # 定义一个函数，输入一个链表的头结点，反转该链表并输出反转后链表的头结点
 
 
@@ -6,12 +7,14 @@ class ListNode(object):
     def __init__(self, x):
         self.val = x
         self.next = None
+
     def __repr__(self):
         if self:
             return "{} -> {}".format(self.val, repr(self.next))
 
+
 class Solution(object):
-    def reverseList(self, head):
+    def reverse_list(self, head):
         res = None
         while head is not None:
             next_node = head.next
@@ -24,11 +27,12 @@ class Solution(object):
             head = next_node
         return res
 
+
 if __name__ == '__main__':
     head = ListNode(None)
-#    head = ListNode(1)
-#    head.next = ListNode(2)
-#    head.next.next = ListNode(3)
-#    head.next.next.next = ListNode(4)
-#    head.next.next.next.next = ListNode(5)
-    print(Solution().reverseList(head))
+    #    head = ListNode(1)
+    #    head.next = ListNode(2)
+    #    head.next.next = ListNode(3)
+    #    head.next.next.next = ListNode(4)
+    #    head.next.next.next.next = ListNode(5)
+    print(Solution().reverse_list(head))
