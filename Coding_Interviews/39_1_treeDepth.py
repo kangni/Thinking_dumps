d1408af9749e214ae044bb75f0e15ee94f3db541
Kang <2881570+kangni@users.noreply.github.com>
2016-10-07 16:21:33 +0800

    add something

--- Coding_Interviews/39_1_treeDepth.py
+++ Coding_Interviews/39_1_treeDepth.py
@@ -1,10 +1,20 @@
-# coding: utf-8
+# -*- coding: utf-8 -*-
 
-'''
-输入一棵二叉树的根结点，求最大树深
-'''
+# 输入一棵二叉树的根结点，求最大树深
 
-def treeDepth(root):
+
+class Node(object):
+    def __init__(self, data, left=None, right=None):
+        self.data = data
+        self.left = left
+        self.right = right
+
+
+def tree_depth(root):
     if not root:
         return 0
-    return max(treeDepth(root.left), treeDepth(root.right)) + 1
+    return max(tree_depth(root.left), tree_depth(root.right)) + 1
+
+if __name__ == '__main__':
+    tree = Node(5, Node(3, Node(2, Node(1), Node(4)), Node(10, Node(7), Node(12))))
+    print(tree_depth(tree))
