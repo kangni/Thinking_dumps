236ddc972b2ca3dad5c1f1ff81ec52d336e8881a
Kang <2881570+kangni@users.noreply.github.com>
2016-10-07 16:22:15 +0800

    add note

--- Coding_Interviews/40_NumbersAppearOnce.py
+++ Coding_Interviews/40_NumbersAppearOnce.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 
+# 数组中只出现一次的数字
+
+# 一个整型数组里除了两个数字之外，其他的数字都出现了两次，输出只出现一次的俩数字
+
 numbers_appear_once = lambda data: list([x for x in data if data.count(x) == 1])
 
 if __name__ == '__main__':
