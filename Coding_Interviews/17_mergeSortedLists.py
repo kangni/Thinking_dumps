ac902103ff3f4c130c349573e9cd516929afb405
Kang <2881570+kangni@users.noreply.github.com>
2016-10-07 10:17:25 +0800

    PEP8 & bug fixed

--- Coding_Interviews/17_mergeSortedLists.py
+++ Coding_Interviews/17_mergeSortedLists.py
@@ -1,13 +1,12 @@
-# coding: utf-8
+# -*- coding: utf-8 -*-
 
-'''
-合并两个排序的链表
+# 合并两个排序的链表
 
-输入两个递增排序的链表，合并这两个链表并使新链表中的结点仍然是按照递增排序的。
-'''
+# 输入两个递增排序的链表，合并这两个链表并使新链表中的结点仍然是按照递增排序的。
+
 
 # 循环算法
-def mergeSortedLists(l1, l2):
+def merge_sorted_lists(l1, l2):
     tmp = []
     while len(l1) > 0 and len(l2) > 0:
         if l1[0] < l2[0]:
@@ -15,15 +14,20 @@ def mergeSortedLists(l1, l2):
             del l1[0]
         else:
             tmp.append(l2[0])
-            ded l2[0]
+            del l2[0]
     tmp.extend(l1)
     tmp.extend(l2)
     return tmp
 
+if __name__ == '__main__':
+    list1 = [1, 2, 5, 7, 8]
+    list2 = [3, 4, 5, 7, 9]
+    print(merge_sorted_lists(list1, list2))
+
 
 # 尾递归
 '''
-def _mergeSortedLists(l1, l2, tmp):
+def _merge_sorted_lists(l1, l2, tmp):
     if len(l1) == 0 or len(l2) == 0:
         tmp.extend(l1)
         tmp.extend(l2)
@@ -35,8 +39,8 @@ def _mergeSortedLists(l1, l2, tmp):
         else:
             tmp.append(l2[0])
             del l2[0]
-    return mergeSortedLists(l1, l2, tmp)
+    return merge_sorted_lists(l1, l2, tmp)
 
-def mergeSortedLists(l1, l2):
-    retuen _mergeSortedLists(l1, l2, [])
+def merge_sorted_lists(l1, l2):
+    retuen _merge_sorted_lists(l1, l2, [])
 '''
