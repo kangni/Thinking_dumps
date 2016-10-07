04b671fdd354d68172c740c474870666bbd2d7d2
Kang <2881570+kangni@users.noreply.github.com>
2016-10-07 16:49:21 +0800

    PEP8

--- Coding_Interviews/49_stringToInt.py
+++ Coding_Interviews/49_stringToInt.py
@@ -1,12 +1,9 @@
-# coding: utf-8
+# -*- coding: utf-8 -*-
 
-'''
-把字符串转换成整数，即实现atoi
-
-'''
+# 把字符串转换成整数，即实现atoi
 
 
-def strToInt(str):
+def str_to_int(a_str):
     INT_MAX = 2147483647
     INT_MIN = -2147483648
     result = 0
@@ -25,10 +22,14 @@ def strToInt(str):
         sign = -1
         i += 1
 
-    while i < len(str) and str[i] >= '0' and str[i] <= '9':
+    while i < len(str) and '0' <= str[i] <= '9':
         if result > (INT_MAX - (ord(str[i]) - ord('0'))) / 10:
             return INT_MAX if sign > 0 else INT_MIN
         result = result * 10 + ord(str[i]) - ord('0')
         i += 1
 
     return sign * result
+
+if __name__ == '__main__':
+    a_string = "abc"
+    print(str_to_int(a_string))
