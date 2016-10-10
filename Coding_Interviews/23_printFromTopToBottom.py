e591db85791e025f2bedc3ee8aa407f000dac269
Kang <2881570+kangni@users.noreply.github.com>
2016-10-10 09:54:15 +0800

    last commit are wrong, this time fixed it

--- Coding_Interviews/23_printFromTopToBottom.py
+++ Coding_Interviews/23_printFromTopToBottom.py
@@ -18,10 +18,10 @@ def lookup(root):
     while stack:
         cur = stack.pop(0)
         print(cur.data)
-        if cur.left:
-            stack.append(cur.left)
         if cur.right:
             stack.append(cur.right)
+        if cur.left:
+            stack.append(cur.left)
 
 if __name__ == '__main__':
     tree = Node(5, Node(3, Node(1), Node(4)), Node(10, Node(7), Node(12)))
