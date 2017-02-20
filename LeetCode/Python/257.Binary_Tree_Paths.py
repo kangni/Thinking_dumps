50c64e92d2f550b1af66ec2f4a8b4c3146057abc
Kang <2881570+kangni@users.noreply.github.com>
2017-02-20 15:50:29 +0800

    improved

--- LeetCode/Python/257.Binary_Tree_Paths.py
+++ LeetCode/Python/257.Binary_Tree_Paths.py
@@ -16,10 +16,10 @@ All root-to-leaf paths are:
 
 
 class TreeNode(object):
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
+    def __init__(self, data, left=None, right=None):
+        self.data = data
+        self.left = left
+        self.right = right
 
 
 class Solution(object):
@@ -32,10 +32,15 @@ class Solution(object):
         if root is None:
             return 0
         else:
-            path.append(root.val)
+            path.append(root.data)
             if root.left is None and root.right is None:
                 p = '->'.join(map(str, path))
                 result.append(p)
             self.binary_tree_paths_recu(root.left, path, result)
             self.binary_tree_paths_recu(root.right, path, result)
             path.pop()
+
+
+if __name__ == "__main__":
+    tree = TreeNode(1, TreeNode(2, TreeNode(5)), TreeNode(3))
+    print(Solution().binary_tree_paths(tree))
