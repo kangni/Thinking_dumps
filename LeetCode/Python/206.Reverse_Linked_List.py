820b8726930b151f14cd75e30f8b6a5a164aba94
Kang <2881570+kangni@users.noreply.github.com>
2017-02-20 15:57:16 +0800

    PEP8

--- LeetCode/Python/206.Reverse_Linked_List.py
+++ LeetCode/Python/206.Reverse_Linked_List.py
@@ -11,6 +11,7 @@ class ListNode(object):
     def __init__(self, x):
         self.val = x
         self.next = None
+
     def __repr__(self):
         if self:
             return "{} -> {}".format(self.val, repr(self.next))
