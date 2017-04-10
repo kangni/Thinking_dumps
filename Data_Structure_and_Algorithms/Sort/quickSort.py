312d4adee2908561d26d828c371c8fbb32e61f5d
Kang <2881570+kangni@users.noreply.github.com>
2017-04-10 18:35:29 +0800

    fix the type error, WTF

--- Data_Structure_and_Algorithms/Sort/quickSort.py
+++ Data_Structure_and_Algorithms/Sort/quickSort.py
@@ -1,13 +1,13 @@
-def quickSearch(seq):
+def quick_sort(seq):
     if seq == []:
         return []
     else:
         pivot = seq[0]
-        lesser = quickSearch([x for x in seq[1:] if x < pivot])
-        greater = quickSearch([x for x in seq[1:] if x >= pivot])
+        lesser = quick_sort([x for x in seq[1:] if x < pivot])
+        greater = quick_sort([x for x in seq[1:] if x >= pivot])
         return lesser + [pivot] + greater
 
 
 if __name__ == '__main__':
     seq = [4, 5, 67, 34, 1, 0, -45, -1, 87, -12]
-    print(quickSearch(seq))
+    print(quick_sort(seq))
