46e2c12b58fb3388f77664f79a70b361bf77e32d
Kang <2881570+kangni@users.noreply.github.com>
2017-06-20 19:13:24 +0800

    imporved

--- Data_Structure_and_Algorithms/Sort/shortBubbleSort.py
+++ Data_Structure_and_Algorithms/Sort/shortBubbleSort.py
@@ -3,10 +3,10 @@ def short_bubble_sort(l):
     短冒泡，若一次遍历没有发生交换，证明列表有序，程序提前停止
     '''
     exchanges = True
-    pass_num = len(l) - 1
+    pass_num = len(l)
     while pass_num > 0 and exchanges:
         exchanges = False
-        for i in range(pass_num):
+        for i in range(pass_num-1):
             if l[i] > l[i + 1]:
                 exchanges = True
                 l[i], l[i + 1] = l[i + 1], l[i]
