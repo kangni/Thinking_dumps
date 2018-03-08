8c7faafc1ba23e9d5efc9d367cdc94b4679e06ce
Kang <2881570+kangni@users.noreply.github.com>
2018-03-08 13:21:13 +0800

    DONE.

--- Yet_Another_Scheme_Tutorial/3_Making_Lists.scm
+++ Yet_Another_Scheme_Tutorial/3_Making_Lists.scm
@@ -18,3 +18,22 @@
 
 ; 6. ("Sum of" (1 2 3 4) "is" 10)
 (cons "Sum of" (cons (cons 1 (cons 2 (cons 3 (cons 4 '())))) (cons "is" (cons 10 '()))))
+
+
+; Exercise 2
+; Evaluated following S-expressions.
+
+; 1. (car '(0))
+0
+
+; 2. (cdr '(0))
+()
+
+; 3. (car '((1 2 3) (4 5 6)))
+(1 2 3)
+
+; 4. (cdr '(1 2 3 . 4))
+(2 3 . 4)
+
+; 5. (cdr (cons 3 (cons 2 (cons 1 '()))))
+(2 1)
